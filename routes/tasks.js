4e9c5c167ef570cb7ea6fb75b7a324ec92fd9531
Ryan Bush <ryan.bush3320@gmail.com>
2023-03-13 09:21:12 -0600

    adding delay to task APIs

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,58 +6,68 @@ let tasks = [];
 
 // Add task
 router.post('/:id', (req, res) =>{
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send('The name of the task is required and minimum of 3 characters long.');
-        return;
-    } 
-
-    const task = {
-        id: tasks.length + 1,
-        name: req.body.name
-    };
-    tasks.push(task);
-    res.send(tasks);
+    setTimeout(() =>{
+        if(!req.body.name || req.body.name.length < 3){
+            res.status(400).send('The name of the task is required and minimum of 3 characters long.');
+            return;
+        } 
+    
+        const task = {
+            id: tasks.length + 1,
+            name: req.body.name
+        };
+        tasks.push(task);
+        res.send(tasks);
+    }, 500);
 });
 
 // Return One Task
 router.get('/:id', (req, res) =>{
-    const task = tasks.find(c => c.id === parseInt(req.params.id));
-    if(!task) res.status(404).send('That task was not found');
-
-    res.send(task);
+    setTimeout(() =>{
+        const task = tasks.find(c => c.id === parseInt(req.params.id));
+        if(!task) res.status(404).send('That task was not found');
+    
+        res.send(task);
+    }, 500);
 });
 
 // Return All Tasks
 router.get('/', (req, res) =>{
-    res.send(tasks);
+    setTimeout(() =>{
+        res.send(tasks);
+    }, 500);
 })
 
 // Update Task
 router.put('/:id', (req, res) =>{
-    const task = tasks.find(c => c.id === parseInt(req.params.id));
-    if(!task) res.status(404).send('That task was not found');
-
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send('The name of the task is required and minimum of 3 characters long.');
-        return;
-    }
-
-    task.name = req.body.name;
-    res.send(tasks);
+    setTimeout(() =>{
+        const task = tasks.find(c => c.id === parseInt(req.params.id));
+        if(!task) res.status(404).send('That task was not found');
+    
+        if(!req.body.name || req.body.name.length < 3){
+            res.status(400).send('The name of the task is required and minimum of 3 characters long.');
+            return;
+        }
+    
+        task.name = req.body.name;
+        res.send(tasks);
+    }, 500);
 });
 
 // Delete Task
 router.delete('/:id', (req, res) =>{
-    //Find task
-    const task = tasks.find(c => c.id === parseInt(req.params.id));
-    if(!task) res.status(404).send('That task was not found');
-
-    //Delete task
-    const index = tasks.indexOf(task);
-    tasks.splice(index, 1);
-
-    //Return
-    res.send(tasks);
+    setTimeout(() =>{
+        //Find task
+        const task = tasks.find(c => c.id === parseInt(req.params.id));
+        if(!task) res.status(404).send('That task was not found');
+    
+        //Delete task
+        const index = tasks.indexOf(task);
+        tasks.splice(index, 1);
+    
+        //Return
+        res.send(tasks);
+    }, 500);
 });
 
 module.exports = router;
