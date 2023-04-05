773d4e9d98ea2ad9176f9077b309e315e9152476
Ryan Bush <ryan.bush3320@gmail.com>
2023-04-05 12:56:35 -0600

    Test authentication added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const users = require('./routes/users');
 const db = require('./routes/db-tasks');
+const auth = require('./routes/auth');
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`app: ${app.get('env')}`);
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/tasks/inmemory', tasks);
 app.use('/api/tasks', db);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 
 const port = process.env.PORT || 3000;
--- routes/auth.js
+++ routes/auth.js
@@ -11,14 +11,12 @@ router.post('/', async (req, res) =>{
     // if(error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if(user) return res.status(400).send('Already registered');
+    if(!user) return res.status(400).send('Invalid email or password');
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password');
 
-    await user.save();
-    res.send(_.pick(user, ['name', 'email']));
+    res.send(true);
 });
 
 module.exports = router;
